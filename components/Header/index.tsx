3aa23934664ffda1804bed5f0956591e470e6f09
PValtrich <pdasilvavaltrich@gmail.com>
2024-11-28 12:08:28 -0300

    28/11

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Icon } from 'react-native-elements';
-import { Link as ExpoRouterLink } from 'expo-router';
 
 export default function Header() {
     return (
         <StyledHeader>
-
             <TittleHeader>Química InBox</TittleHeader>
-
-            <ButtonLink href='/(login)'>
-                <Icon
-                    type="material"
-                    name='person'
-                    size={35}
-                    color={'#636C76'}
-                />
-            </ButtonLink>  
         </StyledHeader>
     );
 }
@@ -30,25 +18,9 @@ const StyledHeader = styled.View`
     padding: 0 10px;
 `;
 
-const IconContainer = styled.View`
-    width: 35px;
-    height: 35px;
-    align-items: center;
-    justify-content: center;
-`;
-
 const TittleHeader = styled.Text`
     flex: 1;
     font-size: 20px;
     color: #636C76;
     text-align: center;
-    margin-right: -30px;
-`;
-
-const ButtonLink = styled(ExpoRouterLink)`
-    width: 35px;
-    height: 35px;
-    align-items: center;
-    justify-content: center;
-    margin-left: 10px;
 `;
